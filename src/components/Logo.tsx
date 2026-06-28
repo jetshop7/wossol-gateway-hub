5c5e582ff6d6fb403a4414816f93207354753604
jetshop7 <lalaboulala8@gmail.com>
2026-06-28 17:23:39 +0100

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -16,7 +16,7 @@ export function Logo({ className = "", imageClassName = "" }: LogoProps) {
       <img
         src={logo}
         alt="Wossol Export"
-        className={`h-10 w-auto object-contain md:h-12 ${imageClassName}`}
+        className={`h-12 w-auto object-contain md:h-14 ${imageClassName}`}
       />
     </Link>
   );
